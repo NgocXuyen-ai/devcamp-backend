4b261e419b7c44be87ef623ce32383b8de25eae9
tranminhkhoi8407-dev <tranminhkhoi8407@gmail.com>
2026-06-17 21:54:47 +0700

    Save local workspace changes

--- src/exercises/exercises.module.ts
+++ src/exercises/exercises.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ExercisesController } from './exercises.controller';
+import { ExercisesService } from './exercises.service';
+import { Submission, SubmissionSchema } from './schemas/submission.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Submission.name, schema: SubmissionSchema },
+    ]),
+  ],
+  controllers: [ExercisesController],
+  providers: [ExercisesService],
+  exports: [ExercisesService],
+})
 export class ExercisesModule {}
